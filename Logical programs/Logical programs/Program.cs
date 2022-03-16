a8d7695eb9e6c40d18b9facb62de5d83b7fb1aad
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-16 18:39:42 +0530

    Uc5 : Coupon Numbers

--- Logical programs/Logical programs/Program.cs
+++ Logical programs/Logical programs/Program.cs
@@ -1,21 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Reverse a Number");
+Console.WriteLine("Coupon Numbers");
 
-int num = 0;
-int temp = 0;
-int remainder = 0; 
-int reverse = 0;
+Random random = new Random();
 
-Console.WriteLine("Enter an integer \n");
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-num = int.Parse(Console.ReadLine());
+var stringChars = new char[15];
 
-temp = num;
-while (num > 0)
+for (int i = 0; i < stringChars.Length; i++)
 {
-    remainder = num % 10;
-    reverse = reverse * 10 + remainder;
-    num /= 10;
+    stringChars[i] = chars[random.Next(chars.Length)];
+    Console.WriteLine(" Coupon Number " + stringChars[i]);
 }
-Console.WriteLine("Given number is = {0}", temp);
-Console.WriteLine("Its reverse is = {0}", reverse);
