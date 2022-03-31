df68418e7b6312a612c563c88fe8d57bdb406b01
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-31 16:56:31 +0530

    Uc6 : Stopwatch

--- Logical programs/Logical programs/Program.cs
+++ Logical programs/Logical programs/Program.cs
@@ -1,14 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Coupon Numbers");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
 
-Random random = new Random();
-
-var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-var stringChars = new char[15];
-
-for (int i = 0; i < stringChars.Length; i++)
+namespace LogicalPrograms
 {
-    stringChars[i] = chars[random.Next(chars.Length)];
-    Console.WriteLine(" Coupon Number " + stringChars[i]);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating new stopwatch object
+            Stopwatch stopwatch = new Stopwatch();
+
+            //Setting start timming of stopwatch by calling method start();
+            stopwatch.Start();
+
+            //For-loop
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(1);
+            }
+            //Setting stop timming of stopwatch by calling method stop();
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed:{0}", stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    }
 }
